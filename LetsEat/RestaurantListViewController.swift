a4596e0339b13277a42c7acc2e55da944d8a5e5f
gabriel.rodrigues <gabriel.rodrigues@apesoftware.com.br>
2017-05-20 22:18:50 -0300

    Inicio a listagem dos restaurantes.

---
+++ LetsEat/RestaurantListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RestaurantListViewController.swift
+//  LetsEat
+//
+//  Created by Gabriel Rodrigues on 20/05/17.
+//  Copyright © 2017 Apê Software. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+        
+        return cell
+    }
+    
+}
